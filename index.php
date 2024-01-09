76a42a04c8569434ac4d694b5525f5cb94d35f48
Igor <71745720+Igor475@users.noreply.github.com>
2024-01-09 10:32:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once("conexao.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/logo-IBMM-preta.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="css/login.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap_login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
     <title><?php echo $nome_igreja ?></title>
