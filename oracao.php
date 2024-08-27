be4c07873ec9265461b70c233ae15ee7095e0381
Igor de Matos Ferreira <igor.ferreira@netcpad.com.br>
2024-08-27 09:29:35 -0300

    Adicionando arquivo individual para os scripts da oração

--- oracao.php
+++ oracao.php
@@ -57,62 +57,10 @@ $id_igreja = @$_SESSION['id_igreja'];
 
     </form>
 
-    <script type="text/javascript">
-        $("#form_prayer").submit(function () {
-            event.preventDefault();
-            var formData = new FormData(this);
+    <script type="text/javascript" src="js/oracao.js"></script>
+    <script type="text/javascript" src="js/mascaras.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
-            $.ajax({
-                url: "http://localhost/IBMM_admin/painel-igreja/oracao/inserir.php",
-                type: 'POST',
-                data: formData,
-
-                success: function (mensagem) {
-                    $('#mensagem').text('');
-                    $('#mensagem').removeClass()
-                    if (mensagem.trim() == "Salvo com Sucesso") {
-
-                        oracaoSalvar();
-                        setTimeout(function () {
-                            window.location = "oracao.php";
-                        }, 2500);
-
-                    } else {
-
-                        $('#mensagem').addClass('message_error')
-                        $('#mensagem').text(mensagem)
-                    }
-
-
-                },
-
-                cache: false,
-                contentType: false,
-                processData: false,
-
-            });
-
-        });
-
-
-        function oracaoSalvar(mensagem) {
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
-            });
-            Toast.fire({
-                icon: "success",
-                title: "Sua oração foi enviada co sucesso!"
-            });
-        }
-    </script>
 </body>
 
 </html>
